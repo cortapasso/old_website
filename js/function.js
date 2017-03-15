0c2bcd8f79770321a0dd69f899c360348ecfe33b
John <cortapasso28@gmail.com>
2017-03-15 16:13:48 -0300

    Added all the files
    project unfinished

---
+++ js/function.js
@@ -0,0 +1,8 @@
+$(function() {$("body")
+ .on("input propertychange", ".form-item",function(e) {
+  $(this).toggleClass("form-item-filled",!! $(e.target).val());})
+ .on("focus", ".form-item",function() {
+  $(this).addClass("form-item-focused");})
+ .on("blur", ".form-item",function() {
+  $(this).removeClass("form-item-focused");});
+});
